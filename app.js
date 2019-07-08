61abd29622deffee6d91bc051574fde549674135
Takik Hasan <hasantakik@gmail.com>
2019-07-09 01:34:29 +0600

    Second commit

--- app.js
+++ app.js
@@ -28,25 +28,32 @@ var UIController = (function () {
 
 
 var controller = (function (budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            } 
+        });
+    }
+    
     var ctrlAddItem = function() {
         /// 1. Get the input
         var input = UICtrl.getInput();
-        console.log(input);
         /// 2. Add the item to the bufget controller
         /// 3. Add the item to UI controller
         /// 4. Calculate the budget
         /// 5. Display the budget on the UI
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-        
-    });
-
+    }
 })(budgetController, UIController);
+
+
+controller.init();
