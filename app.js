64414c04be57a3bc91d9abf8d9e1b8c7ae9484dd
Takik Hasan <hasantakik@gmail.com>
2019-07-23 19:56:01 +0600

    Clearing the fields

--- app.js
+++ app.js
@@ -83,6 +83,15 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
+        clearFields: function() {
+            var fields;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            fieldsArr[0].focus();
+        },
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -109,6 +118,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetController.addItem(input.type, input.description, input.value);
         /// 3. Add the item to UI controller
         UICtrl.addListItem(newItem, input.type);
+        UICtrl.clearFields();
         /// 4. Calculate the budget
         /// 5. Display the budget on the UI
     }
