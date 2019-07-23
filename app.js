765bce7c56ac04bc7b97f99f3badf0168f55c3ee
Takik Hasan <hasantakik@gmail.com>
2019-07-23 18:28:44 +0600

    Added more functionality

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 
 
 var budgetController = (function () {
-
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+            }
+            else {
+                ID = 0;
+            }
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            }
+            else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            data.allItems[type].push(newItem);
+            this.testing();
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    }
 })();
 
 
@@ -39,9 +81,11 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
     
     var ctrlAddItem = function() {
+        var input, newItem;
         /// 1. Get the input
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         /// 2. Add the item to the bufget controller
+        newItem = budgetController.addItem(input.type, input.description, input.value);
         /// 3. Add the item to UI controller
         /// 4. Calculate the budget
         /// 5. Display the budget on the UI
